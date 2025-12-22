3ebf426cc62b3e2d793b70ae594b4e353d8e1feb
anazif <nazifabdulganiu@gmail.com>
2025-12-22 02:41:40 +0000

    feat: Add homepage update with intentional syntax bug

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return '''
-    <h1>DevOps Docker Project</h1>
+    <h1>Devops Docker Project - BUG VERSION</h1>
     <p>Built by: Anazif Abdulganiu</p>
-    <p>Tech Stack: Python + Flask + Docker</p>
-    <p>Status: Running in container!</p>
-    <p>Hostname: {}</p>
-    '''.format(os.uname().nodename)
+    <p>Status:This version has a syntax bug!</p>
+    <p>Python: {}</p>
+    <p>This line is missing closing quote: #BUG HERE
+    '''.format(os.popen('python --version').read().strip())
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000)
-
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
